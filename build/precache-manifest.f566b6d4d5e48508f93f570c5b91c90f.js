f2e5bc15343c9444478acf94dda1073b6d7d0255
hongthang152 <nguyenhongthang1998@gmail.com>
2019-03-23 22:07:12 -0400

    Upload build file

---
+++ build/precache-manifest.f566b6d4d5e48508f93f570c5b91c90f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b18a00e2b6680d584683",
+    "url": "/static/js/main.dcf87301.chunk.js"
+  },
+  {
+    "revision": "4f536cf765340b500384",
+    "url": "/static/js/2.ffed1280.chunk.js"
+  },
+  {
+    "revision": "b18a00e2b6680d584683",
+    "url": "/static/css/main.25276ec9.chunk.css"
+  },
+  {
+    "revision": "8d23f1f2cfa758dd3fd98f542891b268",
+    "url": "/index.html"
+  }
+];
